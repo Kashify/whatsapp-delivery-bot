d114bccb2ce8ca88f0833bedc10876cb288c7d1c
Kashish Chaudhary <kashishchaudhary@Kashishs-Mac-mini.local>
2026-03-27 03:24:42 +0530

    fix webhook

--- server.js
+++ server.js
@@ -87,12 +87,16 @@ app.post("/webhook", async (req, res) => {
 
 // ===== VERIFY WEBHOOK =====
 app.get("/webhook", (req, res) => {
-  if (req.query["hub.verify_token"] === process.env.VERIFY) {
-    return res.send(req.query["hub.challenge"]);
-  }
-  res.sendStatus(403);
-});
+  const VERIFY_TOKEN = process.env.VERIFY;
 
-app.listen(process.env.PORT, () => {
-  console.log("🚀 Server running");
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("✅ Webhook verified");
+    return res.status(200).send(challenge);
+  } else {
+    return res.sendStatus(403);
+  }
 });
